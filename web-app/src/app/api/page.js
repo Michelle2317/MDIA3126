949dede105aab35ed0d7a7f3edea7474dbef4c19
Michelle2317 <michelle388q@gmail.com>
2024-10-20 13:41:09 -0700

    setting up supabase

--- web-app/src/app/api/page.js
+++ web-app/src/app/api/page.js
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Cherry_Swash } from "next/font/google";
-import { createClient } from "@/utils/supabase/clients";
 
 const font = Cherry_Swash({
     weight: "700",
@@ -11,25 +10,10 @@ const font = Cherry_Swash({
 });
 
 export default function Page() {
-    const supabase = createClient();
-    const [session, setSession] = useState();
     const [products, setProducts] = useState(null);
+
     const API_URL =
         "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
-
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session);
-        });
-
-        const {
-            data: { subscription },
-        } = supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-
-        return () => subscription.unsubscribe;
-    }, []);
 
     async function fetchProducts() {
         try {
@@ -65,14 +49,6 @@ export default function Page() {
 
         return <div className='text-6xl'>🌭🫛🍖🫘🥫</div>;
     };
-
-    if (session === null) {
-        return <div>Log in...</div>;
-    }
-
-    if (session === undefined) {
-        return <div>Loading...</div>;
-    }
 
     return (
         <div className='p-4 bg-yellow-300'>
